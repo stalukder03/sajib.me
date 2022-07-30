b21e7f493059f6230b9cf76dc00e0e63ac93d6ad
stalukder03 <stalukder03@gmail.com>
2022-07-30 17:18:42 +0600

    Templae created for loaidng react app

---
+++ wp-content/themes/sajib-me/tmp-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Home / Rest API  
+ * 
+ */
+// get_header();
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sajib React</title>
+</head>
+<body>
+    <div id="__react_sajib_me">Hello World</div>
+</body>
+</html>
