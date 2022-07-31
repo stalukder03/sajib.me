7a89598cfd58e0aff5e674034739a374d9a683e1
stalukder03 <stalukder03@gmail.com>
2022-07-31 09:25:19 +0600

    Cleanup code

--- wp-content/themes/sajib-me/react-functions.php
+++ wp-content/themes/sajib-me/react-functions.php
@@ -1,35 +1,6 @@
 <?php
 
 
-function get_site_data() {
-	
-	// Site Info
-	$site_info = [
-		'name'      => get_bloginfo('name'),
-		'tag_title' => get_bloginfo('description')
-	];
-
-
-	/**
-	 * Menu List
-	 */
-	// Get nav locations set in theme, usually functions.php) // returns an array of menu locations ([LOCATION_NAME] = MENU_ID);
-	$menuLocations = get_nav_menu_locations();
-    $menuID = $menuLocations['primary']; // Get the *primary* menu added in register_nav_menus()
-    $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
-	$tempMenu = [];
-	foreach( $primaryNav as $menu ){
-		$new_menu = ['id' => $menu->ID, 'title' => $menu->title, 'url' => $menu->url];
-		array_push($tempMenu,$new_menu);
-	}
-    return [
-		'site_info'         => $site_info,
-		'menu'              => $tempMenu,
-		'user_profile'      => get_user_profile()
-	];
-}
-
-
 function get_user_profile($id = 1) {
     $user_id = $id;
     $user    = get_user_by( 'id', $user_id );
@@ -51,6 +22,42 @@ function get_user_profile($id = 1) {
         'attachment_id'  => $post_thumbnail_id,
         'attachment_url' => $attachment_url,
     );
+}
+
+
+function get_site_info() {
+    return [
+		'name'      => get_bloginfo('name'),
+		'tag_title' => get_bloginfo('description')
+	];
+}
+
+
+function get_menu_list() {
+
+	// Get nav locations set in theme, usually functions.php) // returns an array of menu locations ([LOCATION_NAME] = MENU_ID);
+	$menuLocations = get_nav_menu_locations();
+    $menuID = $menuLocations['primary']; // Get the *primary* menu added in register_nav_menus()
+    $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
+	$tempMenu = [];
+	foreach( $primaryNav as $menu ){
+		$new_menu = ['id' => $menu->ID, 'title' => $menu->title, 'url' => $menu->url];
+		array_push($tempMenu,$new_menu);
+	}
+
+    return $tempMenu;
+}
+
+function get_site_data() {
+	
+	/**
+	 * Menu List
+	 */
+    return [
+		'site_info'         => get_site_info(),
+		'menu'              => get_menu_list(),
+		'user_profile'      => get_user_profile()
+	];
 }
 
 
